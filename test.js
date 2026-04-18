0410356d81bd5b5cc11af4112ae0d263cec673ba
gionacvlishvili2004-arch <gio.nacvlishvili2004@gmail.com>
2026-04-18 21:57:33 +0400

    test

---
+++ test.js
@@ -0,0 +1,26 @@
+let a = 87;
+let b = 65;
+console.log(a + b);
+
+let student = {
+  firstname: 'Giorgi',
+  lastname: 'Natsvlishvili',
+  age: 21,
+  email: 'GioN@gmail.com',
+  Marks: [8, 6, 4, 12]
+};
+
+console.log(student.firstname);
+console.log(student.Marks[2]);
+
+{
+  let a = 23;
+  let b = a / 2;
+  let c = a + b;
+  console.log(c);
+}
+
+let phones = ['Iphone', 'Samsung', 'Pixel', 'Huawei', 'Xiaomi'];
+console.log(phones);
+console.log(phones[0]);
+console.log(phones[phones.length - 1]);
